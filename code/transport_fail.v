670f7b78f76afbb5449e11d2f3005fe80ebf9d4e
Edith <edith_lzh@sjtu.edu.cn>
2022-03-20 19:43:32 +0800

    transport_fail

--- code/transport_fail.v
+++ code/transport_fail.v
@@ -19,27 +19,12 @@ module transport_fail(
     input clk, enable, rst_n,
     input [1:0] dout_x, dout_y, dout_local,
     input [2:0] fail,
-    output reg [1:0] dout_x_, dout_y_, dout_local_);
+    output [1:0] dout_x, dout_y, dout_local);
 
-wire [1:0] dout_x_0, dout_y_0, dout_local_0;
 
-transport_fail_unit tfuX(clk, enable, rst_n, dout_x, fail[2], dout_x_0);
-transport_fail_unit tfuY(clk, enable, rst_n, dout_y, fail[1], dout_y_0);
-transport_fail_unit tfuLOCAL(clk, enable, rst_n, dout_local, fail[0], dout_local_0);
-
-always @(posedge clk or posedge rst_n) begin
-    if (rst_n) begin
-        // reset
-        dout_x_ <= 2'b00;
-        dout_y_ <= 2'b00;
-        dout_local_ <= 2'b00;
-    end
-    else if (enable) begin
-        dout_x_ <= dout_x_0;
-        dout_y_ <= dout_y_0;
-        dout_local_ <= dout_local_0;
-    end
-end
+transport_fail_unit tfuX(clk, enable, rst_n, dout_x, fail[2], dout_x);
+transport_fail_unit tfuY(clk, enable, rst_n, dout_y, fail[1], dout_y);
+transport_fail_unit tfuLOCAL(clk, enable, rst_n, dout_local, fail[0], dout_local);
 
 //here use two registers because it takes one cycle for judge.v to calculate fail signal
 
